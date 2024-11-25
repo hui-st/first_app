1690b2c34c1a19b6248323b1d445a52eea96247e
hui-st <huivingt@gmail.com>
2024-11-26 00:33:52 +0900

    Update s.py

--- s.py
+++ s.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
-#import geopandas as gpd
+import matplotlib.pyplot as plt
+import geopandas as gpd
 
 # 타이틀 텍스트 출력
 st.title('**Choropleth지도 시각화(시군구)**')
